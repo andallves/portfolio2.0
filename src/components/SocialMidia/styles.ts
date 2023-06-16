0b2893d571f3c4698f358c05a4bdc3ab9a811bec
andallves <andrealves10a@gmail.com>
2023-06-16 10:17:14 -0300

    editing a style by SocialMidia component

--- src/components/SocialMidia/styles.ts
+++ src/components/SocialMidia/styles.ts
@@ -1,16 +1,29 @@
-import styled,  { css } from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Div = styled.div`
+export const Ul = styled.ul`
   ${({ theme }) => css`
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
-    width: 12.1rem;
+    width: 10.1rem;
+    list-style: none;
 
-    > span {
-      font-size: ${theme.sizes.small};
-      color: ${theme.colors.whiteColor}
+    > li {
+      margin: 0 1rem;
+
+      > a {
+        > svg {
+          fill: ${theme.colors.whiteColor};
+          font-size: ${theme.sizes.small};
+          cursor: pointer;
+          transition: all 0.4s;
+
+          &:hover {
+            fill: ${theme.colors.gradientColor}; /* fallback for old browsers */
+          }
+        }
+      }
     }
   `}
 `;

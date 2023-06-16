3781cc36a419f4196fa4d728ec0b6b416d4cf57c
andallves <andrealves10a@gmail.com>
2023-06-15 22:05:46 -0300

    create a MenuLink component

---
+++ src/components/MenuLink/index.tsx
@@ -0,0 +1,11 @@
+import * as Styled from './styles';
+
+export const MenuLink = () => {
+  return (
+    <Styled.Menu>
+      <a href='#'>Projects</a>
+      <a href='#'>Technologies</a>
+      <a href='#'>About me</a>
+    </Styled.Menu>
+  )
+}
---
+++ src/components/MenuLink/styles.ts
@@ -0,0 +1,22 @@
+import styled, { css } from 'styled-components';
+
+export const Menu = styled.div`
+   ${({ theme }) => css`
+      width: 45rem;
+      position: relative;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+
+      > a {
+        font-family: ${theme.fontFamily.default};
+        font-style: normal;
+        font-weight: 500;
+        font-size: ${theme.sizes.small};
+        line-height: 4.5rem;
+
+        color${theme.colors.whiteColor};
+      }
+   `}
+`;

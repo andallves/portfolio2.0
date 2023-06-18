b2f8d0d2e063c4c528c56225bebcb77ef45eb38e
andallves <andrealves10a@gmail.com>
2023-06-18 14:09:09 -0300

    editin Button styles

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,30 +1,18 @@
 import styled, { css } from 'styled-components';
 
-
 export const Button = styled.button`
   ${({ theme }) => css`
-    
-
-    background: #00d2ff; /* fallback for old browsers */
-    background: -webkit-linear-gradient(
-      to right,
-      #928dab,
-      #00d2ff
-    ); /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(
-      to right,
-      #928dab,
-      #00d2ff
-    ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
+    width: 13rem;
+    height: 4rem;
+    background: #24c6dc;
+    background: -webkit-linear-gradient(to right, #514a9d, #24c6dc);
+    background: linear-gradient(to right, #514a9d, #24c6dc);
     border-radius: 5rem;
-
     font-family: ${theme.fontFamily.default};
     font-style: normal;
-    font-weight: 600;
-    font-size: ${theme.sizes.small};
+    font-weight: 400;
+    font-size: ${theme.sizes.xsmall};
     line-height: 4.2rem;
-
     color: ${theme.colors.whiteColor};
   `}
 `;

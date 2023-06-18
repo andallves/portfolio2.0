3a58428eebd5e1f5282b56e774adccbe0683c83e
andallves <andrealves10a@gmail.com>
2023-06-18 14:07:44 -0300

    Add new layout components

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,12 +1,20 @@
+import { Projects } from '../template/Projects';
 import { Header } from '../layout/Header';
 import { Navbar } from '../layout/Navbar/';
 import * as Styled from './styles';
+import { Technologies } from '../template/Technologies';
+import { AboutMe } from '../template/AboutMe';
+import { Footer } from '../layout/Footer';
 
 function App() {
    return (
       <Styled.Main>
          <Navbar />
          <Header />
+         <Projects />
+         <Technologies />
+         <AboutMe />
+         <Footer />
       </Styled.Main>
    );
 }

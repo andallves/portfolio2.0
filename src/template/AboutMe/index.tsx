6030dc7d588a160539c9583c0e5b5c2e8d32e913
andallves <andrealves10a@gmail.com>
2023-06-20 14:08:39 -0300

    Add the AboutImage and Paragraph in the AboutMe

--- src/template/AboutMe/index.tsx
+++ src/template/AboutMe/index.tsx
@@ -1,10 +1,24 @@
+import { AboutImage } from '../../components/AboutImage';
 import { Heading } from '../../components/Heading';
 import * as Styled from './styles';
 
 export const AboutMe = () => {
   return (
-    <Styled.Section>
+    <Styled.Section id='about'>
       <Heading>About me</Heading>
+
+      <Styled.Container>
+        <AboutImage />
+
+        <Styled.P>
+          Quando eu tive o primeiro contato com a programação me identifiquei
+          muito, e desde então venho buscando aprender novas tecnologias e
+          praticando diariamente, dessa forma sinto-me opto para contribuir,
+          assim busco minha primeira oportunidade. Sou um pouco tímido mas gosto
+          de trabalhar em equipe e prezo por uma boa comunicação, pois acredito
+          que a troca de experiência é sempre valida.
+        </Styled.P>
+      </Styled.Container>
     </Styled.Section>
   );
 };
--- src/template/AboutMe/styles.ts
+++ src/template/AboutMe/styles.ts
@@ -2,10 +2,29 @@ import styled, { css } from 'styled-components';
 
 export const Section = styled.section`
   ${() => css`
+    width: 90%;
     display: flex;
     flex-direction: column;
     align-items: start;
     justify-items: flex-start;
     margin: 5rem auto;
+    padding: 2rem;
   `}
+`;
+
+export const Container = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 5rem;
+  margin: 5rem auto;
+`;
+
+export const P = styled.p`
+  position: relative;
+  font-size: 1.6rem;
+  margin: 2rem 5rem 3rem;
+  padding: 4rem;
 `;

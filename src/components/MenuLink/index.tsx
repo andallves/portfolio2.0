092d1fcf74f904eda1fa343935c2404dbbdd8951
andallves <andrealves10a@gmail.com>
2023-06-20 11:47:58 -0300

    Add id to menu link

--- src/components/MenuLink/index.tsx
+++ src/components/MenuLink/index.tsx
@@ -3,8 +3,8 @@ import * as Styled from './styles';
 export const MenuLink = () => {
   return (
     <Styled.Menu>
-      <a href='#'>Projects</a>
-      <a href='#'>Technologies</a>
+      <a href='#projects'>Projects</a>
+      <a href='#technologies'>Technologies</a>
       <a href='#'>About me</a>
     </Styled.Menu>
   )

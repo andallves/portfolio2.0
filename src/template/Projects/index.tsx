531b8ad48b5b7f7b8498c1f9a962a869dfa618fa
andallves <andrealves10a@gmail.com>
2023-06-20 14:09:09 -0300

    Sending props with value to Project compoenent

--- src/template/Projects/index.tsx
+++ src/template/Projects/index.tsx
@@ -1,16 +1,41 @@
 import { Heading } from '../../components/Heading';
 import { Project } from '../../components/Project';
+import TodoImage from '../../assets/imgs/project-todo.png';
+import MultiStepFormImage from '../../assets/imgs/project-multistepform.png';
+import AgendaImage from '../../assets/imgs/project-agenda.png';
 import * as Styled from './styles';
 
 export const Projects = () => {
   return (
-    <Styled.Section id='projects'>
+    <Styled.Section id="projects">
       <Heading>Projects</Heading>
 
       <div>
-        <Project />
-        <Project />
-        <Project />
+        <Project
+          projectImg={TodoImage}
+          title={'Todo'}
+          description={
+            'No projeto Todo é uma lista de afazeres, onde é possível fazer um CRUD'
+          }
+          link={'https://github.com/andallves/todo'}
+        />
+        <Project
+          projectImg={MultiStepFormImage}
+          title={'Formulário MultiStep'}
+          description={
+            'Um formulário com várias etapas onde é possível pegar um questionário extenso e dividir-lo de acordo com as perguntas e só no final ele é enviado.'
+          }
+          link={'https://github.com/andallves/challenge_MultStepForm'}
+        />
+        <Project
+          projectImg={AgendaImage}
+          title={'Agenda de Contatos'}
+          description={
+            'Nessa agenda, você precisa fazer o cadastro, depois acessa seu login para poder salvar contatos, excluir, editar... CRUD'
+          }
+          link={'https://github.com/andallves/projetoagenda'}
+
+        />
       </div>
     </Styled.Section>
   );

e40abcaad9a5506980368ee3bfc38f101eb8270e
andallves <andrealves10a@gmail.com>
2023-06-20 14:51:29 -0300

    :up:

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -5,17 +5,17 @@ export const Footer = () => {
   return (
     <Styled.Footer>
       <Styled.ContainerContato>
-      <Styled.ContainerField>
-        <p>Call me:</p>
-        <p>(85) 9927-94032</p>
-      </Styled.ContainerField>
-      <Styled.ContainerField>
-        <p>Email:</p>
-        <p>andrealves10a@gmail.com</p>
-      </Styled.ContainerField>
+        <Styled.ContainerField>
+          <p>Call me:</p>
+          <p>(85) 9927-94032</p>
+        </Styled.ContainerField>
+        <Styled.ContainerField>
+          <p>Email:</p>
+          <p>andrealves10a@gmail.com</p>
+        </Styled.ContainerField>
       </Styled.ContainerContato>
 
-      <SocialMidia isBigger={true}/>
+      <SocialMidia isBigger={true} />
     </Styled.Footer>
   );
 };

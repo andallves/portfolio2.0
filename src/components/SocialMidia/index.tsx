a35d7e82874c74f703ecce0ce6624a68104d0cd8
andallves <andrealves10a@gmail.com>
2023-06-20 14:09:33 -0300

    Add link to SocialMidia elements

--- src/components/SocialMidia/index.tsx
+++ src/components/SocialMidia/index.tsx
@@ -9,17 +9,17 @@ export const SocialMidia = ({isBigger}: SocialMidiaProps) => {
   return (
     <Styled.Ul isBigger={isBigger}>
       <li>
-        <a>
+        <a href='https://github.com/andallves' target='_blank'>
           <BsGithub />
         </a>
       </li>
       <li>
-        <a>
+        <a href='https://www.linkedin.com/in/andre-alves-pereira-02738114b/' target='_blank'>
           <BsLinkedin />
         </a>
       </li>
       <li>
-        <a>
+        <a href='https://www.instagram.com/andallves/' target='_blank'>
           <BsInstagram />
         </a>
       </li>
